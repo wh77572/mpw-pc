b1927f56371c1cfb112e897a91f017742f1741b8
lemmo <wh77572@163.com>
2026-06-16 21:37:41 +0800

    feat: 首次提交

---
+++ config/defaultSettings.ts
@@ -0,0 +1,26 @@
+import type { ProLayoutProps } from '@ant-design/pro-components';
+
+/**
+ * @name
+ */
+const Settings: ProLayoutProps & {
+  logo?: string;
+} = {
+  navTheme: 'light',
+  colorPrimary: '#1677ff',
+  layout: 'top',
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  fixSiderbar: false,
+  colorWeak: false,
+  title: '米鹏财税',
+  logo: '/logo.svg',
+  iconfontUrl: '',
+  footerRender: false,
+  token: {
+    // 参见ts声明，demo 见文档，通过token 修改样式
+    //https://procomponents.ant.design/components/layout#%E9%80%9A%E8%BF%87-token-%E4%BF%AE%E6%94%B9%E6%A0%B7%E5%BC%8F
+  },
+};
+
+export default Settings;
